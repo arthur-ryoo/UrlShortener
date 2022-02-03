3ecb3a96fc2f130cf1639a5229c88097bc0c6a83
Arthur Ryoo <jene0523@gmail.com>
2022-02-02 23:54:35 -0800

    Create util functions

---
+++ utils/utils.js
@@ -0,0 +1,24 @@
+function generateRandomUrl(length) {
+  let url = '';
+  let characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < length; i++) {
+    url += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return url;
+}
+
+function validateUrl(url) {
+  var pattern = new RegExp(
+    '^(https?:\\/\\/)?' +
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+      '((\\d{1,3}\\.){3}\\d{1,3}))' +
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+      '(\\?[;&a-z\\d%_.~+=-]*)?' +
+      '(\\#[-a-z\\d_]*)?$',
+    'i'
+  );
+  return !!pattern.test(url);
+}
+
+module.exports = { generateRandomUrl, validateUrl };
