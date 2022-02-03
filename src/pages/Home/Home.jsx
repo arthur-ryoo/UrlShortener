1cf5ab9be7cf1849d4e41651f82f64af75612b7d
Arthur Ryoo <jene0523@gmail.com>
2022-02-02 23:51:49 -0800

    Create home page component

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UrlShortener from '../../components/UrlShortener/UrlShortener';
+
+function Home() {
+  return (
+    <>
+      <UrlShortener />
+    </>
+  );
+}
+
+export default Home;
